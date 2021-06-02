7d911a29d4fbbb2ceedb40c43e93457ec19f7d74
Ramrup-mahato <ramrupmahato1996@gmail.com>
2021-06-02 18:24:12 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner'
+import Nav from './components/Nav'
+import Services from './components/Services'
+import Description from './components/Description'
+import AboutMe from './components/AboutMe'
+import Animated from './components/Animated'
+import Contact from './components/Contact'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Banner />
+      <Services />
+      <Description />
+      <AboutMe />
+      <Animated />
+      <Contact />
+      <Form />
+      
     </div>
   );
 }
