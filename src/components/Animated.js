759ab3fc3b6ee48e4a6a023ee7383e5111016d4e
Ramrup-mahato <ramrupmahato1996@gmail.com>
2022-11-30 15:28:03 +0530

    some changes

--- src/components/Animated.js
+++ src/components/Animated.js
@@ -9,9 +9,7 @@ const Animated = () => {
                     <div className='col-ani col-ani-1'>
                         <h3>I'M EXPERT ON</h3>
                         <h1>Let's Work Together</h1>
-                        <p className='para'>Phasellus accumsan scelerisque dolor, quis mattis justo bibendum non. Nulla 
-                            sollicitudin turpis in enim elementum varius. Vestibulum ante ipsum
-                             primis in faucibus orci luctus et ultrices posuere cubilia Curae</p>
+                        <p className='para'>I have created on of project using HTML , CSS ,JAVASCRIPT . also i have created some project on react js</p>
                         <a href='' >Hide me now</a>
                     </div>
                     <div className='col-ani clearfix col-ani-2'>
@@ -37,7 +35,7 @@ const Animated = () => {
                     <div className='col-ani clearfix'>
                         <p>JAVASCRIPT</p>
                         <div >
-                            <div className='message3'><FaCommentAlt /> 80%</div>
+                            <div className='message3'><FaCommentAlt /> 80%</div> 
                             <div className='skill-bar'>
                                 <div style={{width:'80%'}}></div>
                             </div>
@@ -55,7 +53,7 @@ const Animated = () => {
                         </div>
                     </div> 
                     <div className='col-ani clearfix'>
-                        <p>CORE JAVA</p>
+                        <p>Next.js</p>
                         <div >
                             <div className='message5'><FaCommentAlt /> 60%</div>
                             <div className='skill-bar'>
